5c09f31aaf9d203f75150fb58cb482ee78983f20
msprijatelj <44978587+msprijatelj@users.noreply.github.com>
2022-04-12 13:09:59 -0400

    feat(Items): add docs for new item generators

--- _run_all.bat
+++ _run_all.bat
@@ -1,3 +1,9 @@
 start feat.exe
 start power.exe
 start npc.exe
+start basic_armors.exe
+start basic_items.exe
+start basic_weapons.exe
+start magic_armors.exe
+start magic_items.exe
+start magic_weapons.exe
